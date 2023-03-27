738970684011ae352d5b7b256d50d38b0e0bc022
Dieter Walker <103620023+DieterWalker@users.noreply.github.com>
2023-03-28 01:23:30 +0700

    Add files via upload

---
+++ python/JS/UI.js
@@ -0,0 +1,14 @@
+function OpenLogin(){
+    document.getElementById('Login').style.display = "block";
+    document.getElementById('Register').style.display = "none";
+}
+
+function OpenRegister(){
+    document.getElementById('Register').style.display = "block";
+    document.getElementById('Login').style.display = "none";
+}
+
+function ExitLog(){
+    document.getElementById('Login').style.display = "none";
+    document.getElementById('Register').style.display = "none";
+}
